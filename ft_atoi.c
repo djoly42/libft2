5bb881c52d1008e251a0a91759aa8400cc38444d
Your Name <you@example.com>
2016-04-19 14:05:34 +0200

    ft_atoi et makefile

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 21:17:06 by djoly             #+#    #+#             */
-/*   Updated: 2016/03/25 15:57:08 by djoly            ###   ########.fr       */
+/*   Updated: 2016/04/15 11:18:57 by djoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,18 @@
 #include <limits.h>
 #include <stdlib.h>
 
-int	ft_atoi(const char *str)
+static int	ft_atoi_error(long long number, int neg)
+{
+	int		i;
+
+	i = 0;
+	if (number > ((long long)INT_MAX + 1) || (number == ((long long)INT_MAX + 1)
+	&& !neg))
+		return (0);
+	return (1);
+}
+
+int			ft_atoi(const char *str)
 {
 	int			neg;
 	long long	number;
@@ -31,12 +42,12 @@ int	ft_atoi(const char *str)
 		i++;
 	while (str[i] >= 48 && str[i] <= 57)
 	{
+		if (ft_nbrlen(number) > 10)
+			exit(ft_putstr("Error: des paramètres plus grand que l'int\n"));
 		number = number * 10;
-		number = number + (str[i] - 48);
-		i++;
+		number = number + (str[i++] - 48);
 	}
-	if (number > ((long long)INT_MAX + 1) || (number == ((long long)INT_MAX + 1)
-	&& !neg))
+	if (ft_atoi_error(number, neg) == 0)
 		exit(ft_putstr("Error: des paramètres plus grand que l'int\n"));
 	if (neg)
 		return (-number);
